e1ea8844211783d18b981c949d829929203dc94c
Russ <qatsby@yandex.com>
2023-07-17 22:23:18 -0400

    new POST method added

--- src/test/java/Praktikum.java
+++ src/test/java/Praktikum.java
@@ -2,6 +2,9 @@ import io.restassured.RestAssured;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import io.restassured.response.Response;
+import java.io.File;
+
 
 // дополнительный статический импорт нужен, чтобы использовать given(), get() и then()
 import static io.restassured.RestAssured.*;
@@ -30,10 +33,35 @@ public class Praktikum {
     //Проверяем имя пользователя
     @Test
     public void checkUserName() {
-        given()
-                .auth().oauth2("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGI0YTZkYzQ1M2NkYzAwNDJmZjNkMWYiLCJpYXQiOjE2ODk1NjA3OTcsImV4cCI6MTY5MDE2NTU5N30.4HrDvmX8boSU1XOF9PeWyu-QhNWvw5IT71O7cmrwgLw")
+        given().auth().oauth2("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGI0YTZkYzQ1M2NkYzAwNDJmZjNkMWYiLCJpYXQiOjE2ODk1NjA3OTcsImV4cCI6MTY5MDE2NTU5N30.4HrDvmX8boSU1XOF9PeWyu-QhNWvw5IT71O7cmrwgLw")
                 .get("api/users/me")
                 .then().assertThat().body("data.name", equalTo("Жак-Ив Кусто"));
     }
 
+    ////Проверяем имя пользователя  и выводим результат на экран
+    @Test
+    public void checkUserNameAndPrintResponseBody() {
+        // отправляет запрос и сохраняет ответ в переменную response, экзмепляр класса Response
+        Response response = given().auth().oauth2("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGI0YTZkYzQ1M2NkYzAwNDJmZjNkMWYiLCJpYXQiOjE2ODk1NjA3OTcsImV4cCI6MTY5MDE2NTU5N30.4HrDvmX8boSU1XOF9PeWyu-QhNWvw5IT71O7cmrwgLw").get("/api/users/me");// проверяет, что в теле ответа ключу name соответствует нужное имя пользователя
+        response.then().assertThat().body("data.name", equalTo("Жак-Ив Кусто"));
+        // выводит тело ответа на экран
+        System.out.println(response.body().asString());
+    }
+
+    //Добавляем новую карточку с POST  запросом
+    @Test
+    public void createNewPlaceAndCheckResponse() {
+        //Конструктор класса который связывает файл на компьютере и объект класса file
+        File json = new File("src/test/resources/newCard.json");
+        given()
+                .header("Content-type", "application/json")
+                .auth().oauth2("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGI0YTZkYzQ1M2NkYzAwNDJmZjNkMWYiLCJpYXQiOjE2ODk1NjA3OTcsImV4cCI6MTY5MDE2NTU5N30.4HrDvmX8boSU1XOF9PeWyu-QhNWvw5IT71O7cmrwgLw")
+                .and()
+                .body(json)
+                .when()
+                .post("/api/cards")
+                .then().statusCode(201);
+    }
 }
+
+
