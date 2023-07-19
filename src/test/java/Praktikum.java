9c41443685bf47ca2df7d5e6a843fc5babab4828
Russ <qatsby@yandex.com>
2023-07-18 22:11:03 -0400

    new methods adedd

--- src/test/java/Praktikum.java
+++ src/test/java/Praktikum.java
@@ -1,8 +1,10 @@
 import io.restassured.RestAssured;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import io.restassured.response.Response;
+
 import java.io.File;
 
 
@@ -31,20 +33,34 @@ public class Praktikum {
                 .then().statusCode(200);
     }
 
-    //Проверяем имя пользователя
+    //Проверяем имя пользователя используя метод GET
     @Test
     public void checkUserName() {
         given().auth().oauth2("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGI0YTZkYzQ1M2NkYzAwNDJmZjNkMWYiLCJpYXQiOjE2ODk1NjA3OTcsImV4cCI6MTY5MDE2NTU5N30.4HrDvmX8boSU1XOF9PeWyu-QhNWvw5IT71O7cmrwgLw")
                 .get("api/users/me")
-                .then().assertThat().body("data.name", equalTo("Жак-Ив Кусто"));
+                .then().assertThat().body("data.name", equalTo("Vasya"));
+    }
+    //Проверяем информацию о пользователе используя метод GET
+    @Test
+    public void checkUserAbout() {
+       Response response =
+               given()
+                .auth().oauth2("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGI0YTZkYzQ1M2NkYzAwNDJmZjNkMWYiLCJpYXQiOjE2ODk1NjA3OTcsImV4cCI6MTY5MDE2NTU5N30.4HrDvmX8boSU1XOF9PeWyu-QhNWvw5IT71O7cmrwgLw")
+                .get("api/users/me");
+                response.then().assertThat().body("data.about", equalTo("QA Automation Engineer"));
+        System.out.println(response.body().asString());
+
+
     }
 
-    ////Проверяем имя пользователя  и выводим результат на экран
+    //Проверяем имя пользователя  и выводим результат на экран
     @Test
     public void checkUserNameAndPrintResponseBody() {
         // отправляет запрос и сохраняет ответ в переменную response, экзмепляр класса Response
-        Response response = given().auth().oauth2("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGI0YTZkYzQ1M2NkYzAwNDJmZjNkMWYiLCJpYXQiOjE2ODk1NjA3OTcsImV4cCI6MTY5MDE2NTU5N30.4HrDvmX8boSU1XOF9PeWyu-QhNWvw5IT71O7cmrwgLw").get("/api/users/me");// проверяет, что в теле ответа ключу name соответствует нужное имя пользователя
-        response.then().assertThat().body("data.name", equalTo("Жак-Ив Кусто"));
+        Response response =
+                given()
+                        .auth().oauth2("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGI0YTZkYzQ1M2NkYzAwNDJmZjNkMWYiLCJpYXQiOjE2ODk1NjA3OTcsImV4cCI6MTY5MDE2NTU5N30.4HrDvmX8boSU1XOF9PeWyu-QhNWvw5IT71O7cmrwgLw").get("/api/users/me");// проверяет, что в теле ответа ключу name соответствует нужное имя пользователя
+        response.then().assertThat().body("data.name", equalTo("Vasya"));
         // выводит тело ответа на экран
         System.out.println(response.body().asString());
     }
@@ -56,15 +72,40 @@ public class Praktikum {
         File json = new File("src/test/resources/newCard.json");
         Response response =
                 given()
-                .header("Content-type", "application/json")
-                .auth().oauth2("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGI0YTZkYzQ1M2NkYzAwNDJmZjNkMWYiLCJpYXQiOjE2ODk1NjA3OTcsImV4cCI6MTY5MDE2NTU5N30.4HrDvmX8boSU1XOF9PeWyu-QhNWvw5IT71O7cmrwgLw")
-                .and()
-                .body(json)
-                .when()
-                .post("/api/cards");
+                        .header("Content-type", "application/json")
+                        .auth().oauth2("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGI0YTZkYzQ1M2NkYzAwNDJmZjNkMWYiLCJpYXQiOjE2ODk1NjA3OTcsImV4cCI6MTY5MDE2NTU5N30.4HrDvmX8boSU1XOF9PeWyu-QhNWvw5IT71O7cmrwgLw")
+                        .and()
+                        .body(json)
+                        .when()
+                        .post("/api/cards");
         response.then().assertThat().body("data._id", notNullValue())
                 .and()
                 .statusCode(201);
+    }
+
+    @Test
+    public void updateUserInformation() {
+        //Передадим json прямо в строке
+        String json = "{\"name\": \"Vasya\", \"about\": \"QA Automation Engineer\"}";
+        Response response =
+                given()
+                        .header("Content-type", "application/json")
+                        .auth().oauth2("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGI0YTZkYzQ1M2NkYzAwNDJmZjNkMWYiLCJpYXQiOjE2ODk1NjA3OTcsImV4cCI6MTY5MDE2NTU5N30.4HrDvmX8boSU1XOF9PeWyu-QhNWvw5IT71O7cmrwgLw")
+                        .and()
+                        .body(json)
+                        .when()
+                        .patch("api/users/me");
+                        response.then().assertThat().body("data.name", equalTo("Vasya"))
+                                .assertThat().body("data.about", equalTo("QA Automation Engineer"))
+                                .and()
+                                .statusCode(200);
+        System.out.println(response.body().asString());
+    }
+
+    //Удаляем карточку
+    @AfterEach
+    public void cleanUp() {
+
     }
 }
 
